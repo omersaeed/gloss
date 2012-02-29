91c2514b878d37b362290c5bc224eee69251b548
Aaron Stacy <astacy@storediq.com>
2012-02-29 19:08:08 +0000

    disabled tvp select when no output volume is selected
    git-svn-id: svn+ssh://svn.storediq.com/var/svn/siq_repo/trunk/deepfiler/parts/ui/djangoweb/static/vendor/gloss@229851 8e5d5800-4c5e-4594-a3ec-261ff659abd1

--- widgets/collectionviewable.js
+++ widgets/collectionviewable.js
@@ -14,24 +14,26 @@ define([
                 if (self.disable) {
                     self.disable();
                 }
-                collection.load(options.collectionLoadArgs).done(function() {
-                    var startingValue = self.getValue();
-                    self.set('models', collection.models);
-                    _.each(self.options.entries, function(entry) {
-                        // use type coercion in case it's an int
-                        if (entry.value == startingValue) {
-                            self.setValue(startingValue);
-                        }
-                    });
-                    if (self.enable) {
-                        self.enable();
-                    }
-
-                    // handle any future updates
-                    collection.on('update', function(evtName, collection) {
+                if (collection) {
+                    collection.load(options.collectionLoadArgs).done(function() {
+                        var startingValue = self.getValue();
                         self.set('models', collection.models);
+                        _.each(self.options.entries, function(entry) {
+                            // use type coercion in case it's an int
+                            if (entry.value == startingValue) {
+                                self.setValue(startingValue);
+                            }
+                        });
+                        if (self.enable) {
+                            self.enable();
+                        }
+
+                        // handle any future updates
+                        collection.on('update', function(evtName, collection) {
+                            self.set('models', collection.models);
+                        });
                     });
-                });
+                }
             }
         }
     };
