5c3f9950228edb1ffb04f4bd4fe2f7e1c1f62233
Aaron Stacy <aaron.r.stacy@gmail.com>
2012-05-04 08:39:22 -0500

    fixed up checkboxgroup tests

--- src/widgets/checkboxgroup.js
+++ src/widgets/checkboxgroup.js
@@ -2,8 +2,9 @@ define([
     'path!vendor:underscore',
     'path!gloss:widgets/formwidget',
     'path!gloss:widgets/collectionviewable',
+    'path!gloss:widgets/checkbox',
     'path!gloss:tmpl!widgets/checkboxgroup/checkboxgroup.mtpl'
-], function(_, FormWidget, CollectionViewable, template) {
+], function(_, FormWidget, CollectionViewable, CheckBox, template) {
     return FormWidget.extend({
         defaults: {
             template: template,
@@ -13,37 +14,43 @@ define([
         },
 
         create: function() {
+            this._super();
             this.update();
         },
 
         getValue: function() {
             return _.filter(
-                this.$node.find('input[type=checkbox]').map(function(i, el) {
-                    return el.checked? el.getAttribute('data-value') : null;
+                _.map(this.checkboxes, function(cb) {
+                    return cb.getValue()? cb.options.value : null;
                 }),
                 _.identity
             );
         },
 
         setValue: function(array) {
-            var stringified = _.map(array, function(i) { return ''+i; });
-            this.$node.find('input[type=checkbox]').each(function(i, el) {
-                if (_.indexOf(stringified, el.getAttribute('data-value')) >= 0) {
-                    el.checked = true;
-                }
+            _.each(this.checkboxes, function(cb) {
+                cb.setValue(_.indexOf(array, cb.options.value) >= 0);
             });
             return this;
         },
 
         updateWidget: function(updated) {
-            var options = this.options;
+            var options = this.options, checkboxes;
 
             if (updated.models) {
                 this.set('entries', _.map(options.models, options.translate));
             }
 
             if (updated.entries) {
-                this.$node.html(template(this));
+                _.each(this.checkboxes || [], function(cb) { cb.destroy(); });
+                this.checkboxes = checkboxes = [];
+                this.$node.html(template(this))
+                    .find('input[type=checkbox]').each(function(i, el) {
+                        checkboxes.push(CheckBox(el, {
+                            value: options.entries[i].value,
+                            name: options.entries[i].name
+                        }));
+                    });
             }
         }
     }, {mixins: [CollectionViewable]});
--- src/widgets/checkboxgroup/test.js
+++ src/widgets/checkboxgroup/test.js
@@ -1,20 +1,53 @@
 /*global test, asyncTest, ok, equal, deepEqual, start, module, strictEqual, notStrictEqual, raises*/
 require([
+    'path!vendor:underscore',
     'path!gloss:widgets/checkboxgroup',
     'path!gloss:data/mock',
     'path!gloss:test/api/v1/targetvolume',
     'path!gloss:text!test/api/v1/test/fixtures/targetvolume.json'
-], function(CheckBoxGroup, Mock, TargetVolume, targetvolume_json) {
+], function(_, CheckBoxGroup, Mock, TargetVolume, targetvolume_json) {
+
+    var valueMatchesCheckboxes = function(checkboxes, value) {
+        _.each(checkboxes, function(cb) {
+            var idx = _.indexOf(value, cb.options.value);
+            equal(cb.node.checked, idx >= 0);
+        });
+    };
 
     Mock(TargetVolume, JSON.parse(targetvolume_json));
 
-    asyncTest('checkbox instantiation', function() {
-        var cbg = window.cbg = CheckBoxGroup()
+    asyncTest('checkbox instantiation from collection', function() {
+        var cbg = CheckBoxGroup()
                     .set('collection', TargetVolume.collection())
-                    .appendTo('body');
+                    .appendTo('#qunit-fixture');
         setTimeout(function() {
-            ok(true);
-            console.log('in there!');
+            equal(cbg.$node.find('input[type=checkbox]').length, 6);
+            start();
+        }, 50);
+    });
+
+    test('checkboxgroup instantiation without collection', function() {
+        var cbg = window.cbg = CheckBoxGroup(undefined, {
+            entries: [
+                {name: 'foo bar baz', value: 0},
+                {name: 'foo bar biggity iggity bazzle', value: 1}
+            ]
+        }).appendTo('#qunit-fixture');
+
+        equal(cbg.$node.find('input[type=checkbox]').length, 2);
+    });
+
+    asyncTest('setting and getting value of checkboxgroup', function() {
+        var cbg = CheckBoxGroup()
+                    .set('collection', TargetVolume.collection())
+                    .appendTo('#qunit-fixture');
+        setTimeout(function() {
+            equal(cbg.getValue().length, 0);
+            valueMatchesCheckboxes(cbg.checkboxes, cbg.getValue());
+
+            cbg.setValue([1357, 4]);
+            ok(_.isEqual(cbg.getValue(), [1357, 4]), 'getValue() should equal [1357, 4]');
+            valueMatchesCheckboxes(cbg.checkboxes, cbg.getValue());
             start();
         }, 50);
     });
