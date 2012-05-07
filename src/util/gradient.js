6c484d44c279d30d88e6a718439862eeb06e6e85
Aaron Stacy <aaron.r.stacy@gmail.com>
2012-05-07 13:24:16 -0500

    fixed bugs in gradient utility

--- src/util/gradient.js
+++ src/util/gradient.js
@@ -1,6 +1,7 @@
 define([
+    'path!vendor:underscore',
     'path!bedrock:class'
-], function(Class) {
+], function(_, Class) {
     return Class.extend({
         hexRe: /^\s*#?(([0-9a-f]{3})|([0-9a-f]{6}))\s*$/i,
 
@@ -16,11 +17,17 @@ define([
         },
 
         colorFromHex: function(hexVal) {
+            var sliceArgs = [[0, 1], [1, 2], [2, 3]];
             hexVal = hexVal[0] === '#'? hexVal = hexVal.slice(1) : hexVal;
+            if (hexVal.length === 6) {
+                sliceArgs = _.map(sliceArgs, function(arg) {
+                    return _.map(arg, function(v) { return v * 2; });
+                });
+            }
             return {
-                r: parseInt(hexVal.slice(0, 2), 16),
-                g: parseInt(hexVal.slice(2, 4), 16),
-                b: parseInt(hexVal.slice(4, 6), 16)
+                r: parseInt(hexVal.slice.apply(hexVal, sliceArgs[0]), 16),
+                g: parseInt(hexVal.slice.apply(hexVal, sliceArgs[1]), 16),
+                b: parseInt(hexVal.slice.apply(hexVal, sliceArgs[2]), 16)
             };
         },
 
@@ -34,21 +41,24 @@ define([
         },
 
         bucketize: function(n, steps) {
-            var i, buckets = this.buckets(steps);
+            var i = 0, buckets = this.buckets(steps);
             while (buckets[i++] < n);
             return buckets[i-1];
         },
 
         interpolate: function(n, steps) {
-            var r, g, b;
+            var r, g, b, fmt = function(n) {
+                return Math.floor(n).toString(16);
+            };
+
             steps = steps == null? this.steps : steps;
-            n = steps == null? n : this.bucketize(steps);
+            n = steps == null? n : this.bucketize(n);
 
             r = this.start.r + ((this.end.r - this.start.r) * n);
             g = this.start.g + ((this.end.g - this.start.g) * n);
             b = this.start.b + ((this.end.b - this.start.b) * n);
 
-            return '#' + r.toString(16) + g.toString(16) + b.toString(16);
+            return '#' + fmt(r) + fmt(g) + fmt(b);
         }
     });
 });
