ca10af9da6b8bbd658c114a426becc7c7819fc13
Ralph Smith <ralphsmith80@gmail.com>
2012-05-21 10:59:59 -0500

    modified bargraph to add multiply bars under one label

--- src/widgets/bargraph/test.js
+++ src/widgets/bargraph/test.js
@@ -1,7 +1,7 @@
 /*global test, asyncTest, ok, equal, deepEqual, start, module */
 define([
     'component!vendor:jquery',
-    './../bargraph'
+    './../multibargraph'
 ], function ($, BarGraph) {
 
     test('Details graph instantiation', function () {
@@ -13,10 +13,23 @@ define([
     test('Details graph visual test', function () {
         var bg = BarGraph(undefined, {
             data: [
-                {name: "Outlook PST", value: 100 },
-                {name: "Zip Archive", value: 25 },
-                {name: "TGZ archive", value: 75 },
-                {name: "WinRAR archive", value: 50 }
+                {name: "Outlook PST", values: [100] },
+                {name: "Zip Archive", values: [25] },
+                {name: "TGZ archive", values: [75] },
+                {name: "WinRAR archive", values: [50] }
+            ]
+        }).appendTo('#qunit-fixture');
+
+        ok(bg);
+    });
+
+    test('Details graph visual test', function () {
+        var bg = BarGraph(undefined, {
+            data: [
+                {name: "Outlook PST", values: [100, 75] },
+                {name: "Zip Archive", values: [25, 80] },
+                {name: "TGZ archive", values: [75, 20] },
+                {name: "WinRAR archive", values: [50, 10] }
             ]
         }).appendTo('#qunit-fixture');
 
@@ -26,10 +39,10 @@ define([
     test('Details graph no animation', function () {
         var bg = BarGraph(undefined, {
             data: [
-                {name: "Outlook PST", value: 100 },
-                {name: "Zip Archive", value: 25 },
-                {name: "TGZ archive", value: 75 },
-                {name: "WinRAR archive", value: 50 }
+                {name: "Outlook PST", values: [100, 75] },
+                {name: "Zip Archive", values: [25, 80] },
+                {name: "TGZ archive", values: [75, 20] },
+                {name: "WinRAR archive", values: [50, 10] }
             ],
             animationDuration: 0
         }).appendTo('#qunit-fixture');
@@ -41,10 +54,10 @@ define([
         var bg = BarGraph().appendTo($('body'));
 
         var data = [
-            {name: "Outlook PST", value: 45 },
-            {name: "Zip Archive", value: 25 },
-            {name: "TGZ archive", value: 80 },
-            {name: "WinRAR archive", value: 110 }
+            {name: "Outlook PST", values: [45, 55] },
+            {name: "Zip Archive", values: [25, 35] },
+            {name: "TGZ archive", values: [80, 90] },
+            {name: "WinRAR archive", values: [110, 120] }
         ];
 
         bg.set('data', data);
