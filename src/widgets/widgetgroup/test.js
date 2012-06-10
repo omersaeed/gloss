d791f97019bdb32c591cad545ef8acfeab95ac08
Aaron Stacy <aaron.r.stacy@gmail.com>
2012-06-10 12:43:03 -0500

    added unit tests for labels in WidgetGroup
    one of these tests is still failing --- need to fix

--- src/widgets/widgetgroup/test.js
+++ src/widgets/widgetgroup/test.js
@@ -41,11 +41,36 @@ define([
         ok(wg.getWidget('selectbox1') instanceof SelectBox);
         ok(wg.getWidget('selectbox2') instanceof SelectBox);
 
-        ok(wg.getWidget('radiogroup1') instanceof RadioGroup);
+        ok(wg.getWidget('radiogroup1') instanceof RadioGroup,
+            'radiogroup correctly widgetized');
 
-        ok(wg.getWidget('togglegroup1') instanceof ToggleGroup);
+        ok(wg.getWidget('togglegroup1') instanceof ToggleGroup,
+            'togglegroup correctly widgetized');
 
-        ok(wg.getWidget('checkboxgroup1') instanceof CheckBoxGroup);
+        ok(wg.getWidget('checkboxgroup1') instanceof CheckBoxGroup,
+            'checkboxgroup correctly widgetized');
+    });
+
+    test('set label "for" attribute', function() {
+        var wg = WidgetGroup($(html2), {widgetize: true});
+
+        ok(wg.$node.find('label:contains("Text box 1 label")').attr('for') != null);
+        equal(wg.getWidget('textbox1').$node.attr('id'),
+            wg.$node.find('label:contains("Text box 1 label")').attr('for'));
+
+        ok(wg.$node.find('label[data-for="radiogroup1:value1"]').attr('for') != null,
+            'radiogroup labels\' "for" attrubute has been set');
+        equal(
+            wg.$node.find('label[data-for="radiogroup1:value1"]').attr('for'),
+            wg.getWidget('radiogroup1').$node.find('[type=radio]').first().attr('id')
+            );
+
+        ok(wg.$node.find('label[data-for="checkboxgroup1:value1"]').attr('for') != null,
+            'checkboxgroup labels\' "for" attrubute has been set');
+        equal(
+            wg.$node.find('label[data-for="checkboxgroup1:value1"]').attr('for'),
+            wg.getWidget('checkboxgroup1').$node.find('[type=checkbox]').first().attr('id')
+            );
     });
 
     start();
