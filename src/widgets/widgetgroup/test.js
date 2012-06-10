70e4767a64a77d8d7981e6b0914670f6c4ffad56
Aaron Stacy <aaron.r.stacy@gmail.com>
2012-06-10 12:25:17 -0500

    added test case for widgetizing descendents

--- src/widgets/widgetgroup/test.js
+++ src/widgets/widgetgroup/test.js
@@ -2,8 +2,19 @@
 define([
     'vendor/jquery',
     './../widgetgroup',
-    'text!./widgetgrouptesttmpl.html'
-], function($, WidgetGroup, html) {
+    './../button',
+    './../checkbox',
+    './../textbox',
+    './../numberbox',
+    './../selectbox',
+    './../radiogroup',
+    './../togglegroup',
+    './../checkboxgroup',
+    'text!./widgetgrouptesttmpl.html',
+    'text!./widgetgrouptestwidgetizedescendents.mtpl'
+], function($, WidgetGroup, Button, CheckBox, TextBox, NumberBox, SelectBox,
+    RadioGroup, ToggleGroup, CheckBoxGroup, html, html2) {
+
     test('correctly group widgets', function() {
         var wg = WidgetGroup($(html), {widgetize: true});
         deepEqual(wg.getValues(), {
@@ -12,5 +23,30 @@ define([
             fieldset2: {bar: 'barvalue'}
         });
     });
+
+    test('correctly widgetize descendents', function() {
+        var wg = WidgetGroup($(html2), {widgetize: true});
+        ok(wg.getWidget('button1') instanceof Button);
+        ok(wg.getWidget('button2') instanceof Button);
+
+        ok(wg.getWidget('checkbox1') instanceof CheckBox);
+
+        ok(wg.getWidget('textbox1') instanceof TextBox);
+        ok(wg.getWidget('textbox2') instanceof TextBox);
+        ok(wg.getWidget('textbox3') instanceof TextBox);
+        ok(wg.getWidget('textbox4') instanceof TextBox);
+
+        ok(wg.getWidget('numberbox1') instanceof NumberBox);
+
+        ok(wg.getWidget('selectbox1') instanceof SelectBox);
+        ok(wg.getWidget('selectbox2') instanceof SelectBox);
+
+        ok(wg.getWidget('radiogroup1') instanceof RadioGroup);
+
+        ok(wg.getWidget('togglegroup1') instanceof ToggleGroup);
+
+        ok(wg.getWidget('checkboxgroup1') instanceof CheckBoxGroup);
+    });
+
     start();
 });
