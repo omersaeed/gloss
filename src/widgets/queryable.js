b1952f6e3acfeb8614de06d2fcbbbd585739b25a
Aaron Stacy <aaron.r.stacy@gmail.com>
2012-06-15 16:29:48 -0500

    fixed initialization bug in queryable

--- src/widgets/queryable.js
+++ src/widgets/queryable.js
@@ -1,34 +1,39 @@
 define([
-    'vendor/jquery'
-], function ($) {
+    'vendor/jquery',
+    'vendor/underscore'
+], function ($, _) {
+    var remove = function(array, obj) {
+        var idx = _.indexOf(array, obj);
+        if (idx >= -1) {
+            array.splice(idx, 1);
+        }
+    };
+
     return {
-        defaults: {
-        },
-        afterInit: function() {
-            var self = this;
-            self.currentDfd = [];
-        },
         executeQuery: function(dfd) {
             var self = this;
 
             dfd = dfd.pipe(
+
                 //Filter Success
                 function(value) {
-                    if(dfd !== self.currentDfd[self.currentDfd.length-1]) {
+                    if(dfd !== _.last(self._currentDfds)) {
+                        remove(self._currentDfds, dfd);
                         return $.Deferred().reject();
                     }
-                    self.currentDfd.length = 0;
+                    // self._currentDfds.length = 0;
+                    remove(self._currentDfds, dfd);
                     return this.resolve();
                 },
 
                 //Filter Failure
                 function(value) {
-                    console.log('failure', value);
+                    remove(self._currentDfds, dfd);
                     return $.Deferred().reject(value);
                 }
             );
 
-            self.currentDfd.push(dfd);
+            (self._currentDfds = self._currentDfds || []).push(dfd);
             return dfd;
         }
     };
