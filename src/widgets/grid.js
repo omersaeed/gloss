d538a81c7a80a833c5c60b968e18f36924809d0f
Muein Muzamil <muein.muzamil@conradlabs.com>
2012-07-26 14:42:37 +0500

    Used prop function of bedrock

--- src/widgets/grid.js
+++ src/widgets/grid.js
@@ -187,8 +187,7 @@ define([
             if (_.isFunction(modelProperty)) {
                 value = modelProperty(model);
             } else {
-                // TODO: update name of this function to match it in bedrock 
-                value = model.getAttributeValue(modelProperty); 
+                value = model.prop(modelProperty); 
             }
             
             return value;
